702285f25b2d7b3c57d78c1e8c46aecd23d5d808
Infamia2334 <53487995+Infamia2334@users.noreply.github.com>
2021-06-02 02:42:56 +0530

    Added static behaviour for style sheets

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ const http = require('http')
 app.use(express.static("public"))
 app.use("/css", express.static(__dirname  + "public/styles"))
 
-
+app.get('/', function(req, res){
+    res.sendFile(__dirname + "/views/register.html")
+})
 
 app.listen(3000, function(){
     console.log("Server started on local port 3000")
