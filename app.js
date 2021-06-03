a5eef3bef046a21d7e8f481bc57687660e47eb9b
Infamia2334 <53487995+Infamia2334@users.noreply.github.com>
2021-06-03 15:54:40 +0530

    Updated fresh pages and link

--- app.js
+++ app.js
@@ -1,15 +1,32 @@
 const express = require("express")
-
 const app = express();
 const http = require('http')
 
 
-app.use(express.static("public"))
+
+//Serving static styles:
+app.use(express.static("public"))                                                                   
 app.use("/css", express.static(__dirname  + "public/styles"))
 
+
+//Setting Views for ejs:
+app.set("views", "./views")
+app.set("view engine", "ejs" )
+
+
 app.get('/', function(req, res){
-    res.sendFile(__dirname + "/views/register.html")
+    res.render("register",{link:"/login"})
 })
+app.get('/login', function(req, res){
+    res.render("signin")
+})
+
+
+
+// app.get('/', function(req, res){
+//     res.sendFile(__dirname + "/views/register.html")
+// })
+
 
 app.listen(3000, function(){
     console.log("Server started on local port 3000")
